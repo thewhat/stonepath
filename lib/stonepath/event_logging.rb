8e2dd5fc529fb105c4eb9936494e60c1e2a5e92b
AJ Henriques <aj@10beat.com>
2014-08-28 16:15:14 -0700

    fix syntax error

--- lib/stonepath/event_logging.rb
+++ lib/stonepath/event_logging.rb
@@ -1,14 +1,12 @@
 module StonePath
   module EventLogging
-
     def log_events
-      has_many :logged_events, :as => :auditable, :class_name => "EventRecord",-> { order "created_at" }
-
+      has_many :logged_events,-> { order "created_at" }, as: :auditable, class_name: "EventRecord"
       define_method :aasm_event_fired do |event_name, old_state_name, new_state_name|
-        self.logged_events.create(:event_name => event_name.to_s,
-                                  :old_state_name => old_state_name.to_s,
-                                  :new_state_name => new_state_name.to_s,
-                                  :user => User.current)
+        self.logged_events.create(event_name: event_name.to_s,
+                                  old_state_name: old_state_name.to_s,
+                                  new_state_name: new_state_name.to_s,
+                                  user: User.current)
       end
     end
   end
